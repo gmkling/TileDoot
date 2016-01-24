c5e0a1b1e7191a2c297ded7a065dc820cc19ef87
Garry Kling <garry.kling@gmail.com>
2016-01-24 00:41:30 -0800

    Added info button, did some work in MainMenu to make layout relative to screenSize. More abstraction needed.

--- TileDoot/SplashScene.swift
+++ TileDoot/SplashScene.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class SplashScene: SKScene {
     
-    var splashImage = SKSpriteNode(imageNamed: "MainMenu1.png")
     let myLabel = SKLabelNode(fontNamed:"Futura-medium")
     var labelBackground = SKSpriteNode()
     
@@ -19,34 +18,44 @@ class SplashScene: SKScene {
     {
         self.backgroundColor = lightGreenTileColor
         // since we need a ptr to the method, this happens here instead of at init time
-        let easyButton = TDButton(defaultImageName: "Red1_def.png", selectImageName: "Red1_sel.png", buttonAction: printStart)
-        let hardButton = TDButton(defaultImageName: "Purple2_def.png", selectImageName: "Purple2_sel.png", buttonAction: printStart)
+        let easyButton = TDButton(defaultImageName: "Purple2_def.png", selectImageName: "Purple2_sel.png", buttonAction: printStart)
+        let hardButton = TDButton(defaultImageName: "Red1_def.png", selectImageName: "Red1_sel.png", buttonAction: printStart)
+        let infoButton = TDButton(defaultImageName: "info1.png", selectImageName: "info1.png", buttonAction: doInfo)
+        
+        let gridSize = self.frame.width/12.0
+        let bigButtonSize = 3.0*gridSize
+        let bigButtonScale = bigButtonSize/500.0
         
         myLabel.text = "TileDoot"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame)*1.33)
         
+//        easyButton.position = CGPoint(x: CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        easyButton.position = CGPoint(x: CGRectGetMidX(self.frame), y:gridSize*11.0)
+        easyButton.setScale(bigButtonScale)
         
-        splashImage.position = CGPoint(x: frame.size.width/2 , y: -frame.size.height/2)
-        splashImage.setScale(1.1)
-        splashImage.zPosition = -1
+        hardButton.position = CGPoint(x: CGRectGetMidX(self.frame), y:gridSize*7.0)
+        hardButton.setScale(bigButtonScale)
         
-        easyButton.position = CGPoint(x: CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        easyButton.setScale(0.25)
-        
-        hardButton.position = CGPoint(x: CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame)-easyButton.frame.size.height)
-        hardButton.setScale(0.25)
+        infoButton.position = CGPoint(x: CGRectGetMidX(self.frame), y: gridSize*2.0)
+        infoButton.setScale(gridSize/500.0) // I want this little one @ 1/12 the width
         
         //self.addChild(splashImage)
         self.addChild(myLabel)
         self.addChild(easyButton)
         self.addChild(hardButton)
+        self.addChild(infoButton)
     }
     
     func printStart()
     {
         print("Start Button pushed.")
         self.backgroundColor = colorBank.randomItem()
+    }
+    
+    func doInfo()
+    {
+        print("Info button pushed.")
     }
     
     // touch overrides may not be needed since we are only interested in buttons.
