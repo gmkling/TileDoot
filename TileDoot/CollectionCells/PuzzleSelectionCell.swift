5f382f3f8c8fd7b5b1bb08fefac989230d12c61b
Garry Kling <garry.kling@gmail.com>
2016-05-09 22:53:13 -0700

    Stale commit - work on the puzzle selection screen.

---
+++ TileDoot/CollectionCells/PuzzleSelectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  PuzzleSelectionCell.swift
+//  TileDoot
+//
+//  Created by Garry Kling on 5/7/16.
+//  Copyright © 2016 Garry Kling. All rights reserved.
+//
+
+import UIKit
+
+class PuzzleSelectionCell: UICollectionViewCell
+{
+    
+}
--- TileDoot/ViewControllers/PuzzleSelectionViewController.swift
+++ TileDoot/ViewControllers/PuzzleSelectionViewController.swift
@@ -39,3 +39,4 @@ class PuzzleSelectionViewController: UIViewController {
     */
 
 }
+
